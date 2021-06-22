4b02039f8d593bb3c3c44b5bee8072dc4072b2fa
Andre Ricardo <andrericardoweb@gmail.com>
2021-06-22 15:37:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlw06_flutter_payflow
+ PayFlow. Projeto da trilha Flutter, NLW 06 - Rocketseat Education
---
+++ android/app/src/main/kotlin/com/example/nlw06_flutter_payflow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nlw06_flutter_payflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
