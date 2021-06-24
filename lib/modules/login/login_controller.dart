9f651ce46d54936b231c4b73b8ecab08ececfbba
Andre Ricardo <andrericardoweb@gmail.com>
2021-06-24 13:29:53 -0300

    Config shared preferences

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:nlw06_flutter_payflow/shared/auth/auth_controller.dart';
+import 'package:nlw06_flutter_payflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -13,7 +14,11 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!,
+        photoURL: response.photoUrl
+      );
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
